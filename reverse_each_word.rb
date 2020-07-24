32d81745fac842cef27fd657da6e4106d5844845
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-24 15:27:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word (sentence)
   array_words = sentence.split
-  processed = array_words.map! { |word| word.reverse }
+  processed = array_words.collect{ |word| word.reverse }
   array_words.join(" ")
 end
