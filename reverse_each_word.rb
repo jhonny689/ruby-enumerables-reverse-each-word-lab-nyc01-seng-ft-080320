9fe2aca095211c9a118345c3365e1a9e77412403
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-24 15:18:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word (sentence)
   array_words = sentence.split
-  array_words.map { |word| word.reverse }
+  array_words.map { |word| "wow #{word}!!" }
   binding.pry 
 end
